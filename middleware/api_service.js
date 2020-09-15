c62fa1a6c7bb4850274d30debfdcb2642d734fba
rifqirizairfansyah <rizairfansyahrifqi@gmail.com>
2020-09-15 10:50:26 +0700

    update features update mac address and excel export

--- middleware/api_service.js
+++ middleware/api_service.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    // baseURL: 'http://abstein-api.pptik.id:3002'
-    baseURL: 'http://localhost:3002'
+    baseURL: 'http://abstein-api.pptik.id:3002'
+    // baseURL: 'http://localhost:3002'
   })
 }
