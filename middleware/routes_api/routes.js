8e32e6c665dfac932d98448f7b56c044d90eb66b
rifqirizairfansyah <rizairfansyahrifqi@gmail.com>
2020-09-28 14:37:13 +0700

    update registrasi sekolah

--- middleware/routes_api/routes.js
+++ middleware/routes_api/routes.js
@@ -54,6 +54,9 @@ export default {
   requestHarianDataSiswa (params, type) {
     return Api().post('/pengguna/' + type + 'PenggunaV2', params)
   },
+  requestSekolahPengguna (params, type) {
+    return Api().post('/general/tambah_akun_sekolah', params)
+  },
   requestJsonSekolah (params, type) {
     return Api().post('/general/' + type + '_MacAddress', params)
   },
